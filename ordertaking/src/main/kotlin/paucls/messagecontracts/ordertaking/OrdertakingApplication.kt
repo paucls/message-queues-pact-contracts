6ff12687d815c487059e233aa6a9117d3604ee81
Paulo Esteban <Paulo.Esteban@emc.com>
2018-07-07 16:24:23 +0100

    Generated two springboot projects for provider and consumer services

---
+++ ordertaking/src/main/kotlin/paucls/messagecontracts/ordertaking/OrdertakingApplication.kt
@@ -0,0 +1,11 @@
+package paucls.messagecontracts.ordertaking
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OrdertakingApplication
+
+fun main(args: Array<String>) {
+    runApplication<OrdertakingApplication>(*args)
+}
---
+++ ordertaking/src/test/kotlin/paucls/messagecontracts/ordertaking/OrdertakingApplicationTests.kt
@@ -0,0 +1,16 @@
+package paucls.messagecontracts.ordertaking
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class OrdertakingApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
