d2cbcbad1609678b60896362dff1c21929628385
Paulo Clavijo Esteban <paucls@gmail.com>
2020-04-28 16:36:14 +0100

    ordertaking - renamed config class

--- ordertaking/src/main/kotlin/paucls/messagecontracts/ordertaking/config/MessageConverterConfig.kt
+++ ordertaking/src/main/kotlin/paucls/messagecontracts/ordertaking/config/MessageConverterConfig.kt
@@ -4,18 +4,12 @@ import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter
 import org.springframework.amqp.support.converter.MessageConverter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Profile
 
-/**
- * RabbitMQ Messaging configuration
- */
 @Configuration
-@Profile("default")
-class RabbitMQConfig {
+class MessageConverterConfig {
 
     @Bean
     fun messageConverter(): MessageConverter {
         return Jackson2JsonMessageConverter()
     }
-
 }
